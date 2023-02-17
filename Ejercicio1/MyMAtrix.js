18653d8993420200747203232e9097a9335d526f
David Bermeo <davidbermeoquimbaya22dbqq@gmail.com>
2023-02-16 21:09:28 -0500

    first commit

---
+++ Ejercicio1/MyMAtrix.js
@@ -0,0 +1,16 @@
+class MyMAtrix {
+    constructor(matrix) {
+      this.matrix = matrix;
+    }
+  
+    metodo1() {
+      // Código del método 1
+    }
+  
+    metodo2() {
+      // Código del método 2
+    }
+  }
+  
+  module.exports = MyMAtrix;
+  
